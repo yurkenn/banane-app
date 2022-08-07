93b91a3b74b4f364eea008e78c8f0b3f0a800c72
oguz yurken <oguz.yurken@gmail.com>
2022-08-07 20:14:59 +0300

    Update Loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,12 +1,26 @@
-import {View, Text} from 'react-native';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
 import React from 'react';
 
 const Loading = () => {
   return (
-    <View>
-      <Text>Loading</Text>
+    <View style={styles.container}>
+      <ActivityIndicator style={styles.loading} size="large" color="white" />
     </View>
   );
 };
 
 export default Loading;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FCD63B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loading: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
